b4014c2929aeb172601e6d9bb8d11ad8c003eaf3
bentttt87 <bentttt87@gmail.com>
2026-09-06 16:12:35 +0700

    Load v55 evidence guard after workflow

--- bundle6.js
+++ bundle6.js
@@ -1,1 +1,1 @@
-document.write('<script src="runtime_patch.js"><\/script><script src="bundle6_core.js"><\/script><script src="workflow.js"><\/script>');
+document.write('<script src="runtime_patch.js"><\/script><script src="bundle6_core.js"><\/script><script src="workflow.js"><\/script><script src="evidence_guard_v55.js"><\/script>');
