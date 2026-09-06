0977980df51c304c7f3b648b8c9f7148bf387090
bentttt87 <bentttt87@gmail.com>
2026-09-06 21:37:28 +0700

    Load Breakage Monitoring security patch v59

--- bundle6.js
+++ bundle6.js
@@ -1,1 +1,1 @@
-document.write('<script src="runtime_patch.js"><\/script><script src="bundle6_core.js"><\/script><script src="workflow.js"><\/script><script src="evidence_guard_v55.js"><\/script>');
+document.write('<script src="runtime_patch.js"><\/script><script src="bundle6_core.js"><\/script><script src="workflow.js"><\/script><script src="evidence_guard_v55.js"><\/script><script src="security_patch_v59.js"><\/script>');
